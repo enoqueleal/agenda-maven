cc496bbb3b40adcea782be9390b895b13f43a528
Enoque Leal <enoquefelipe@gmail.com>
2019-09-05 00:21:50 -0300

    Update database scripts

--- src/main/webapp/database.sql
+++ src/main/webapp/database.sql
@@ -1,20 +1,25 @@
 CREATE TABLE pessoas (
-	id	BIGINT NOT	NULL AUTO_INCREMENT,
+	id	INT NOT	NULL AUTO_INCREMENT,
 	nome			VARCHAR(255),
-	dataNascimento	DATE,
+	data_nascimento	DATE,
+	id_endereco		INT,
+	id_contato		INT,
 	PRIMARY KEY 	(id)
 );
 
 CREATE TABLE enderecos (
-	id	BIGINT NOT	NULL AUTO_INCREMENT,
+	id	INT NOT	NULL AUTO_INCREMENT,
 	logradouro		VARCHAR(255),
 	cep				VARCHAR(255),
 	PRIMARY KEY 	(id)
 );
 
 CREATE TABLE contatos (
-	id	BIGINT NOT	NULL AUTO_INCREMENT,
+	id	INT NOT	NULL AUTO_INCREMENT,
 	email			VARCHAR(255),
 	telefone		VARCHAR(255),
 	PRIMARY KEY 	(id)
 );
+
+ALTER TABLE pessoas ADD CONSTRAINT id_endereco FOREIGN KEY (id_endereco) REFERENCES enderecos (id);
+ALTER TABLE pessoas ADD CONSTRAINT id_contato FOREIGN KEY (id_contato) REFERENCES contatos (id);
